53320d2ef2ec8dd82b33c2f2a699e3b475b8af25
MuhammadKhairunnasNurdin <anasnurdin936@gmail.com>
2026-02-15 21:01:43 +0700

    Refactor(Architecture): change framework from flask to fastApi

--- app/routes/auth/__init__.py
+++
@@ -1,8 +0,0 @@
-## example of how to structure a blueprint for authentication routes, also you have to make files register.py and login.py or other auth related files
-# from flask import Blueprint
-#
-# auth_bp = Blueprint("auth", __name__)
-#
-# from app.routes.auth.register import register
-# from app.routes.auth.login import login
-# and other auth related files
